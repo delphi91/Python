ec2ee75151af0b7690fe323e3ad560743a819bf2
delphi91 <delfina_bujalkowska@wp.pl>
2019-05-17 11:33:39 +0200

    Adding some functions

--- evenOdd.py
+++ evenOdd.py
@@ -1,18 +1,42 @@
 #program prints even and odd numbers of the list
-list = [1, 6, 2, 8, 3 , 4, 56, 745, 221, 9, 64354, 2235, 78532, 6643, 12345, 32, 11, 22, 445, 667, 88]
+import random
+def evenOddList():
+    list = [1, 6, 2, 8, 3 , 4, 56, 745, 221, 9, 64354, 2235, 78532, 6643, 12345, 32, 11, 22, 445, 667, 88]
+    temp = []
+    even = []
+    odd = []
+    for number in list:
+        if number not in temp:
+            temp.append(number)
+    for number in temp:
+        if ( number % 2 == 0):
+            even.append(number)
+        else:
+            odd.append(number)
 
-temp = []
-even = []
-odd = []
-for number in list:
-    if number not in temp:
-        temp.append(number)
-for number in temp:
-    if ( number % 2 == 0):
-     even.append(number)
-    else:
-        odd.append(number)
+    print('Static list:', list, 'Length: ', list.__len__())
+    print('Even numbers:', sorted(even), 'Count: ', even.__len__())
+    print('Odd numbers:', sorted(odd), 'Count: ', odd.__len__())
+#program prints even and odd numbers of random numbers
+def evenOddrandom():
+    list = []
+    for i in random.sample(range(0,100),21):
+        list.append(i)
+    temp = []
+    even = []
+    odd = []
+    for number in list:
+        if number not in temp:
+            temp.append(number)
+    for number in temp:
+        if (number % 2 == 0):
+            even.append(number)
+        else:
+            odd.append(number)
+    print('Dynamic list:', list, 'Length: ', list.__len__())
+    print('Even numbers:', sorted(even), 'Count: ', even.__len__())
+    print('Odd numbers:', sorted(odd), 'Count: ', odd.__len__())
 
-print('List:', list, 'Length: ', list.__len__())
-print('Even numbers:', sorted(even), 'Count: ', even.__len__())
-print('Odd numbers:', sorted(odd), 'Count: ', odd.__len__())
+evenOddList()
+print('\n-------------------------------------------------------\n')
+evenOddrandom()
--- happyNumber.py
+++ happyNumber.py
@@ -1,32 +1,75 @@
 #Program check that user number is happy
 
 def happyNumber(a):
-
-    b = int(a)**2
-    tab = []
-    while b != 1:
-        if b < 9:
-            b=b**2
+    a = int(a)
+    if a == 0:
+        print(a, 'Not happy :(')
+    if a == 1:
+        print(a, 'Happy!')
+    list = []
+    number = a
+    while number != 1:
+        if number < 9:
+            number = number ** 2
+            list.append(number)
         else:
-            temp = str(b)
-            b = 0
-            list = []
-            for i in temp:
-                list.append(i)
-            for j in list:
-                b=b+int(j)**2
-        tab.append(b)
-        tab.sort()
-        for y in tab:
-            if tab.count(y)>1:
-                print(a, ' not the happy number :(')
-                exit()
-    print(a, 'happy number! :)')
+            number = str(number)
+            temp = []
+            for j in number:
+                temp.append(j)
+            number = 0
 
+            for t in temp:
+                number = number + int(t) ** 2
+            list.append(number)
+            if list.count(number) > 1:
+                print(a, 'Not Happy :(')
+                break
+            for l in list:
+                if l == 1:
+                    print(a, 'Happy!:)')
 
+#saving to files- list of random numbers
+def happyNumbers():
+    happyFile = open('happy.txt', 'w+')
+    happyFile.write('File with happy numbers:\n')
+    sadFile = open('sad.txt', 'w+')
+    sadFile.write('File with not happy numbers:\n')
+    for i in range(0,41):
+        if i ==1:
+            print(i, ':D')
+            happyFile.write(str(i)+', ')
+        if i == 0:
+            print(i, ':<')
+            sadFile.write(str(i)+', ')
+            i+=1
+        list = []
+        number = i
+        while number !=1:
+            if number <9:
+                number = number**2
+                list.append(number)
+            else:
+                number = str(number)
+                temp = []
+                for j in number:
+                    temp.append(j)
+                number = 0
+
+                for t in temp:
+                    number = number + int(t)**2
+                list.append(number)
+                if list.count(number)>1:
+                    print(i, ':<')
+                    sadFile.write(str(i)+', ')
+                    break
+                for l in list:
+                    if l ==1:
+                        print(i, ':D')
+                        happyFile.write(str(i)+', ')
+    sadFile.close()
+    happyFile.close()
 print('------Checking happy number------\n')
 a = input('Set the number:\n>>>\t')
 happyNumber(a)
-
-
-
+happyNumbers()
